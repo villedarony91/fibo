6565498a4889509a5faabe0d13bc369cd3b839fa
rlopez <rlopez@tse.org.gt>
2016-08-25 09:37:39 -0600

    fibonacci succeded

--- fibo/Form1.cs
+++ fibo/Form1.cs
@@ -16,5 +16,30 @@ namespace fibo
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = fibonacci(9).ToString();
+        }
+
+         int fibonacci(int num)
+        {
+           
+            if(num == 1)
+            {
+                return 0;
+            }
+            if (num == 2)
+            {
+                return 1;
+            }
+           
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
     }
 }
